3ab1bf682c01d15a2866efc8f9ab5cdbd2d5ca97
Anton Golub <antongolub@antongolub.com>
2023-04-21 12:34:14 +0300

    fix: fix custom publish cmd

--- .releaserc.cjs
+++ .releaserc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   buildCmd: 'yarn build',
-  publishCmd: 'yarn',
+  publishCmd: 'echo "common (default) publish cmd"',
   changelog: 'changelog',
   npmFetch: true,
   npmProvenance: true
